85e0d8204933f18ab5764f01b291efd744895860
martine2025 <flaviomartine@gmail.com>
2025-07-22 15:42:08 -0300

    Update index.js
    teste

--- index.js
+++ index.js
@@ -50,13 +50,12 @@ const visionClient = new vision.ImageAnnotatorClient();
 // Inicializando Google Sheets
 const sheets = google.sheets({
   version: "v4",
-  auth: new google.auth.GoogleAuth({
-    credentials: {
-      client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, "\n")
-    },
-    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-  })
+  const auth = new google.auth.JWT(
+  GOOGLE_SHEETS_CLIENT_EMAIL,
+  null,
+  GOOGLE_SHEETS_PRIVATE_KEY,
+  ['https://www.googleapis.com/auth/spreadsheets']
+);
 });
 
 // Inicializando Nodemailer
